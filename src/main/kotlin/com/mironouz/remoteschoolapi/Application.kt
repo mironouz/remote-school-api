71bbcf24823259a432929ce347c8b47e1865c214
Dmitrii Mironov <mironouz@gmail.com>
2020-04-13 04:07:32 +0200

    enable cors

--- src/main/kotlin/com/mironouz/remoteschoolapi/Application.kt
+++ src/main/kotlin/com/mironouz/remoteschoolapi/Application.kt
@@ -5,6 +5,7 @@ import de.flapdoodle.embed.mongo.distribution.Version
 import org.springframework.boot.WebApplicationType
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.mongo.reactiveMongodb
+import org.springframework.fu.kofu.webflux.cors
 import org.springframework.fu.kofu.webflux.webFlux
 
 val app = application(WebApplicationType.REACTIVE) {
@@ -12,6 +13,9 @@ val app = application(WebApplicationType.REACTIVE) {
     webFlux {
         codecs {
             jackson()
+        }
+        cors {
+            path("/api/**")
         }
     }
     reactiveMongodb {
