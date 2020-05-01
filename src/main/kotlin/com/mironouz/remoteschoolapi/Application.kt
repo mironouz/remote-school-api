4ebce05026f480d2f1fde36b7658a00dae59444b
Dmitrii Mironov <mironouz@gmail.com>
2020-05-01 23:41:56 +0200

    remove cors as reverse proxy is used

--- src/main/kotlin/com/mironouz/remoteschoolapi/Application.kt
+++ src/main/kotlin/com/mironouz/remoteschoolapi/Application.kt
@@ -6,7 +6,6 @@ import de.flapdoodle.embed.mongo.distribution.Version
 import org.springframework.boot.WebApplicationType
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.mongo.reactiveMongodb
-import org.springframework.fu.kofu.webflux.cors
 import org.springframework.fu.kofu.webflux.webFlux
 
 val app = application(WebApplicationType.REACTIVE) {
@@ -15,9 +14,6 @@ val app = application(WebApplicationType.REACTIVE) {
     webFlux {
         codecs {
             jackson()
-        }
-        cors {
-            path("/api/**")
         }
     }
     reactiveMongodb {
