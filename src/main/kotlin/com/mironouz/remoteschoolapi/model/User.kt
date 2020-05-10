c3cc9b6c625540f1ce0fe9a70e8f7ebc7f4e25de
Dmitrii Mironov <mironouz@gmail.com>
2020-05-10 02:32:26 +0200

    do not output password and email of user in json

--- src/main/kotlin/com/mironouz/remoteschoolapi/model/User.kt
+++ src/main/kotlin/com/mironouz/remoteschoolapi/model/User.kt
@@ -1,5 +1,6 @@
 package com.mironouz.remoteschoolapi.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
@@ -7,5 +8,5 @@ import org.springframework.data.mongodb.core.mapping.Document
 data class User(val name: String,
                 val surname: String,
                 val grade: Grade,
-                @Id val email: String,
-                val password: String)
+                @Id @JsonProperty(access=JsonProperty.Access.WRITE_ONLY) val email: String,
+                @JsonProperty(access=JsonProperty.Access.WRITE_ONLY) val password: String)
