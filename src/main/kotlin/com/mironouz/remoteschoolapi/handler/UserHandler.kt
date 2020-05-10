be179f07612e2d499037b840e8a7c74357553331
Dmitrii Mironov <mironouz@gmail.com>
2020-05-10 03:56:36 +0200

    add user to the db only if it does not exist

--- src/main/kotlin/com/mironouz/remoteschoolapi/handler/UserHandler.kt
+++ src/main/kotlin/com/mironouz/remoteschoolapi/handler/UserHandler.kt
@@ -2,6 +2,7 @@ package com.mironouz.remoteschoolapi.handler
 
 import com.mironouz.remoteschoolapi.model.User
 import com.mironouz.remoteschoolapi.repository.UserRepository
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -12,12 +13,11 @@ import reactor.kotlin.core.publisher.switchIfEmpty
 class UserHandler(private val repository: UserRepository, private val userService: MapReactiveUserDetailsService) {
     suspend fun registerUser(request: ServerRequest) : ServerResponse {
         val user = request.awaitBody<User>()
-        var serverResponse = ServerResponse.accepted()
-        repository.save(user)
+        var status = HttpStatus.ACCEPTED
         userService
                 .findByUsername(user.email)
                 .doOnNext {
-                    serverResponse = ServerResponse.status(409)
+                    status = HttpStatus.CONFLICT
                 }
                 .switchIfEmpty {
                     val userDetails = org.springframework.security.core.userdetails.User
@@ -28,7 +28,10 @@ class UserHandler(private val repository: UserRepository, private val userServic
                             .build()
                     userService.updatePassword(userDetails, "{noop}" + user.password)
                 }
-                .block()
-        return serverResponse.buildAndAwait()
+                .subscribe()
+        if (status == HttpStatus.ACCEPTED) {
+            repository.save(user)
+        }
+        return ServerResponse.status(status).buildAndAwait()
     }
 }
